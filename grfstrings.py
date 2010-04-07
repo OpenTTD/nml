1c6b9aa3e7d7d23756d7c4cb57bf664f30627813
Yexo <yexo@openttd.org>
2010-04-07 13:59:25 +0200

    Feature: support for several string codes (todo: action6 support for those codes with parameters)

--- grfstrings.py
+++ grfstrings.py
@@ -1,6 +1,88 @@
-import os, string, codecs
+import os, string, codecs, re
+from generic import ScriptError
 
 grf_strings = {}
+
+escapes = {
+'':               {'escape': r'\n',    'num_params': 0},
+'TINYFONT':       {'escape': r'\0E',    'num_params': 0},
+'BIGFONT':        {'escape': r'\0F',    'num_params': 0},
+'DWORD_S':        {'escape': r'\UE07B', 'num_params': 0},
+'WORD_S':         {'escape': r'\UE07C', 'num_params': 0},
+'BYTE_S':         {'escape': r'\UE07D', 'num_params': 0},
+'WORD_U':         {'escape': r'\UE07E', 'num_params': 0},
+'CURRENCY':       {'escape': r'\UE07F', 'num_params': 0},
+'STRING':         {'escape': r'\UE080', 'num_params': 0},
+'DATE_LONG':      {'escape': r'\UE082', 'num_params': 0},
+'DATE_TINY':      {'escape': r'\UE083', 'num_params': 0},
+'VELOCITY':       {'escape': r'\UE084', 'num_params': 0},
+'POP_WORD':       {'escape': r'\UE085', 'num_params': 0},
+'ROTATE':         {'escape': r'\UE086', 'num_params': 0},
+'VOLUME':         {'escape': r'\UE087', 'num_params': 0},
+'CURRENCY_QWORD': {'escape': r'\UE09A\01', 'num_params': 0},
+'PUSH_WORD':      {'escape': r'\UE09A\03', 'num_params': 1, 'param_size': 2},
+'UNPRINT':        {'escape': r'\UE09A\04', 'num_params': 1, 'param_size': 1},
+'BYTE_HEX':       {'escape': r'\UE09A\06', 'num_params': 0},
+'WORD_HEX':       {'escape': r'\UE09A\07', 'num_params': 0},
+'DWORD_HEX':      {'escape': r'\UE09A\08', 'num_params': 0},
+'QWORD_HEX':      {'escape': r'\UE09A\0B', 'num_params': 0},
+'BLUE':           {'escape': r'\UE088', 'num_params': 0},
+'LIGHT_GRAY':     {'escape': r'\UE089', 'num_params': 0},
+'LIGHT_ORANGE':   {'escape': r'\UE08A', 'num_params': 0},
+'RED':            {'escape': r'\UE08B', 'num_params': 0},
+'PURPLE':         {'escape': r'\UE08C', 'num_params': 0},
+'GRAY_GREEN':     {'escape': r'\UE08D', 'num_params': 0},
+'ORANGE':         {'escape': r'\UE08E', 'num_params': 0},
+'GREEN':          {'escape': r'\UE08F', 'num_params': 0},
+'YELLOW':         {'escape': r'\UE090', 'num_params': 0},
+'LIGHT_GREEN':    {'escape': r'\UE091', 'num_params': 0},
+'RED_BROWN':      {'escape': r'\UE092', 'num_params': 0},
+'BROWN':          {'escape': r'\UE093', 'num_params': 0},
+'WHITE':          {'escape': r'\UE094', 'num_params': 0},
+'LIGHT_BLUE':     {'escape': r'\UE095', 'num_params': 0},
+'DARK_GRAY':      {'escape': r'\UE096', 'num_params': 0},
+'MAUVE':          {'escape': r'\UE097', 'num_params': 0},
+'BLACK':          {'escape': r'\UE098', 'num_params': 0},
+}
+
+def parse_command(command):
+    global escapes
+    match = re.match(r'^([a-zA-Z_]*)(( \d+)*)$', command)
+    if match == None: raise ScriptError("Failed to parse string command: '" + command + "'")
+    cmd_name = match.group(1)
+    arguments = match.group(2).split()
+    if cmd_name not in escapes: raise ScriptError("Unkown string command: '" + cmd_name + "'")
+    escape = escapes[cmd_name]
+    if escape['num_params'] != len(arguments): raise ScriptError("Wrong number of arguments in command: '" + command + "'")
+    ret = escape['escape']
+    if len(arguments) > 0:
+        ret += '" '
+        for arg in arguments:
+            if escape['param_size'] == 1: ret += hex(arg)[2:].upper() + ' '
+            elif escape['param_size'] == 2: ret += '\\w' + arg + ' '
+        ret += '"'
+    return ret
+
+def parse_grf_string(orig_string):
+    ret = ''
+    ignore = 0
+    for i in range(len(orig_string)):
+        if ignore > 0:
+            ignore = ignore - 1
+            continue
+        c = orig_string[i]
+        if c == '{':
+            has_end = False
+            for j in range(i + 1, len(orig_string)):
+                if orig_string[j] == '}':
+                    has_end = True
+                    ret += parse_command(orig_string[i+1:j])
+                    ignore = j - i
+                    break
+            if not has_end: raise ScriptError("Command block without ending '}'")
+        else:
+            ret += c
+    return ret
 
 def read_lang_files():
     for filename in os.listdir("lang/"):
@@ -19,4 +101,4 @@ def read_lang_files():
                 value = line[i+1:-1]
                 if not name in grf_strings:
                     grf_strings[name] = []
-                grf_strings[name].append({'lang': lang, 'text': value})
+                grf_strings[name].append({'lang': lang, 'text': parse_grf_string(value)})
