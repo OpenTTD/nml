8df1a56e1ebbb82bf322abaa3f035b65fced030d
Hirundo <hirundo@openttdcoop.org>
2010-04-12 11:44:27 +0200

    Fix: Last commit contained experimental parser modifications. Revert those.

--- parser.py
+++ parser.py
@@ -43,19 +43,17 @@ def p_skipable_block(t):
     t[0] = t[1]
 
 def p_cargotable(t):
-    'cargotable : CARGOTABLE LBRACE expression_list RBRACE'
+    'cargotable : CARGOTABLE LBRACE cargotable_list RBRACE'
     t[0] = CargoTable(t[3])
 
-
-def p_expression_list(t):
-    '''expression_list : 
-                       | expression
-                       | expression_list COMMA expression'''
+def p_cargotable_list(t):
+    '''cargotable_list : ID
+                       | cargotable_list COMMA ID'''
     if len(t) == 2: t[0] = [t[1]]
     else: t[0] = t[1] + [t[3]]
 
 def p_deactivate(t):
-    'deactivate : DEACTIVATE LPAREN expression_list RPAREN'
+    'deactivate : DEACTIVATE LPAREN NUMBER RPAREN'
     t[0] = DeactivateBlock(t[3])
 
 def p_grf_block(t):
@@ -78,10 +76,16 @@ def p_param_assignment(t):
     t[0] = ParameterAssignment(t[1].num, t[3])
 
 def p_string(t):
-    'string : STRING LPAREN expression_list RPAREN'
+    '''string : STRING LPAREN ID RPAREN
+              | STRING LPAREN ID COMMA param_list RPAREN'''
     if len(t) == 5: t[0] = String(t[3], [])
     else: t[0] = String(t[3], t[5])
 
+def p_param_list(t):
+    '''param_list : expression
+                  | param_list COMMA expression'''
+    if len(t) == 2: t[0] = [t[1]]
+    else: t[0] = t[1] + [t[3]]
 
 def p_const_expression(t):
     'expression : NUMBER'
@@ -91,10 +95,6 @@ def p_param_expression(t):
     'expression : param'
     t[0] = t[1]
 
-def p_id_expression(t):
-    'expression : ID'
-    t[0] = t[1]
-    
 def p_variable_expression(t):
     'expression : variable'
     t[0] = t[1]
@@ -119,13 +119,16 @@ def p_else_block(t):
     elif len(t) == 5: t[0] = Conditional(None, t[3], None)
     else: t[0] = t[2]
 
+def p_feature(t):
+    'feature : NUMBER'
+    t[0] = t[1]
 
 def p_loop(t):
     'loop : WHILE LPAREN expression RPAREN LBRACE skipable_script RBRACE'
     t[0] = Loop(t[3], t[6])
 
 def p_switch(t):
-    'switch : SWITCH LPAREN expression_list RPAREN LBRACE switch_body RBRACE'
+    'switch : SWITCH LPAREN feature COMMA VARSIZE COMMA ID COMMA expression RPAREN LBRACE switch_body RBRACE'
     t[0] = Switch(t[3], t[5], t[7], t[9], t[12])
 
 def p_switch_body(t):
@@ -150,8 +153,10 @@ def p_switch_range(t):
     else: t[0] = SwitchRange(t[1], t[3], t[5])
 
 def p_item(t):
-    'item : ITEM LPAREN expression_list RPAREN LBRACE skipable_script RBRACE'
+    '''item : ITEM LPAREN feature RPAREN LBRACE skipable_script RBRACE
+            | ITEM LPAREN feature COMMA expression RPAREN LBRACE skipable_script RBRACE'''
     if len(t) == 8: t[0] = Item(t[3], t[6])
+    else: t[0] = Item(t[3], t[8], t[5])
 
 def p_property_block(t):
     'property_block : PROPERTY LBRACE property_list RBRACE'
@@ -166,14 +171,16 @@ def p_property_list(t):
 def p_property_assignment(t):
     '''property_assignment : ID COLON expression
                            | ID COLON string
+                           | ID COLON ID
                            | ID COLON array
                            | NUMBER COLON expression
                            | NUMBER COLON string
+                           | NUMBER COLON ID
                            | NUMBER COLON array'''
     t[0] = Property(t[1], t[3])
 
 def p_array(t):
-    'array : LBRACKET expression_list RBRACKET'
+    'array : LBRACKET param_list RBRACKET'
     t[0] = t[2]
 
 def p_graphics_block(t):
@@ -187,11 +194,11 @@ def p_graphics_list(t):
     else: t[0] = t[1].append_definition(t[2])
 
 def p_graphics_assignment(t):
-    'graphics_assignment : ID COLON expression'
+    'graphics_assignment : ID COLON ID'
     t[0] = GraphicsDefinition(t[1], t[3])
 
 def p_spriteblock(t):
-    'spriteblock : SPRITEBLOCK LPAREN expression_list RPAREN LBRACE spriteset_list RBRACE'
+    'spriteblock : SPRITEBLOCK LPAREN feature RPAREN LBRACE spriteset_list RBRACE'
     t[0] = SpriteBlock(t[3], t[6])
 
 def p_spriteset_list(t):
@@ -203,7 +210,7 @@ def p_spriteset_list(t):
     else: t[0] = t[1] + [t[2]]
 
 def p_spriteset(t):
-    'spriteset : SPRITESET LPAREN expression_list RPAREN LBRACE real_sprite_list RBRACE'
+    'spriteset : SPRITESET LPAREN ID COMMA STRING_LITERAL RPAREN LBRACE real_sprite_list RBRACE'
     t[0] = SpriteSet(t[3], t[5], t[8])
 
 def p_real_sprite_list(t):
@@ -230,10 +237,20 @@ def p_spriteview_list(t):
     else: t[0] = t[1] + [t[2]]
 
 def p_spriteview(t):
-    ''' spriteview : ID COLON LBRACKET expression RBRACKET
-                   | ID COLON expression'''
-    if len(t) == 6: t[0] = SpriteView(t[1], t[4])
+    ''' spriteview : ID COLON id_array
+                   | ID COLON ID'''
+    if isinstance(t[3], list): t[0] = SpriteView(t[1], t[3])
     else: t[0] = SpriteView(t[1], [t[3]])
+
+def p_id_list(t):
+    '''id_list : ID
+               | id_list COMMA ID'''
+    if len(t) == 2: t[0] = [t[1]]
+    else: t[0] = t[1] + [t[3]]
+
+def p_id_array(t):
+    'id_array : LBRACKET id_list RBRACKET'
+    t[0] = t[2]
 
 def p_layout_sprite_list(t):
     '''layout_sprite_list : layout_sprite
@@ -254,23 +271,38 @@ def p_layout_param_list(t):
     else: t[0] = t[1] + [t[2]]
 
 def p_layout_param(t):
-    'layout_param : ID COLON expression'
+    '''layout_param : ID COLON ID
+                    | ID COLON expression'''
     t[0] = LayoutParam(t[1], t[3])
 
 #xpos ypos xsize ysize xrel yrel [compression]
 #compression (optional) can either be a number, or one of the following:
 #NORMAL (=default), TILE_COMPRESSION, STORE_COMPRESSED, NORMAL_NOCROP, TILE_COMPRESSION_NOCROP, STORE_COMPRESSED_NOCROP
 def p_real_sprite(t):
-    'real_sprite : LBRACKET expression_list RBRACKET'
-    else: t[0] = RealSprite(t[2])
+    '''real_sprite : LBRACKET sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset RBRACKET
+                   | LBRACKET sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset RBRACKET
+                   | LBRACKET sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset sprite_offset ID RBRACKET'''
+    if len(t) < 10: t[0] = RealSprite(t[2], t[3], t[4], t[5], t[6], t[7])
+    else: t[0] = RealSprite(t[2], t[3], t[4], t[5], t[6], t[7], t[8])
 
+def p_sprite_offset(t):
+    '''sprite_offset : NUMBER
+                    | MINUS NUMBER'''
+    if len(t) == 2: t[0] = t[1]
+    else: t[0] = -t[2]
 
 #severity, message (one of REQUIRES_TTDPATCH, REQUIRES_DOS_WINDOWS, USED_WITH, INVALID_PARAMETER,
 #MUST_LOAD_BEFORE, MUST_LOAD_AFTER, REQUIRES_OPENTTD, or a custom string),
 #data (=string to insert in message), number of parameter, number of parameter
 def p_error_block(t):
-    'error_block : ERROR LPAREN expression_list RPAREN'
-    t[0] = Error(t[3])
+    '''error_block : ERROR LPAREN expression COMMA ID RPAREN
+                   | ERROR LPAREN expression COMMA ID COMMA ID RPAREN
+                   | ERROR LPAREN expression COMMA ID COMMA ID COMMA expression RPAREN
+                   | ERROR LPAREN expression COMMA ID COMMA ID COMMA expression COMMA expression RPAREN'''
+    data = None if len(t) < 8 else t[7]
+    param1 = None if len(t) < 10 else t[9]
+    param2 = None if len(t) < 12 else t[11]
+    t[0] = Error(t[3], t[5], data, param1, param2)
 
 code_to_op = {
     '+' : Operator.ADD,
@@ -303,11 +335,14 @@ def p_binop_plus(t):
     t[0] = BinOp(code_to_op[t[2]], t[1], t[3]);
 
 def p_variable(t):
-    'variable : VARIABLE LBRACKET expression_list RBRACKET'
-    t[0] = Variable(t[3])
+    '''variable : VARIABLE LBRACKET expression RBRACKET
+                | VARIABLE LBRACKET expression COMMA expression RBRACKET'''
+    param = None if len(t) == 5 else t[5]
+    t[0] = Variable(t[3], param)
 
 def p_function(t):
-    '''expression | ID LPAREN expression_list RPAREN'''
+    '''expression : ID LPAREN RPAREN
+                  | ID LPAREN param_list RPAREN'''
     name = t[1]
     args = [] if len(t) == 4 else t[3]
     if name == "min" or name == "max":
