b6d0b21ddbd098883aaa2a7732e372178eead366
Yexo <yexo@openttd.org>
2010-04-20 07:57:32 +0200

    Codechange: move reduce_expr to expression.py

--- nml/ast.py
+++ nml/ast.py
@@ -12,7 +12,6 @@ from actions.actionD import *
 from actions.actionE import *
 from actions.sprite_count import SpriteCountAction
 import global_constants
-import operator
 import unit
 
 def print_script(script, indent):
@@ -52,90 +51,6 @@ class ParameterAssignment:
     
     def get_action_list(self):
         return parse_actionD(self)
-
-# compile-time expression evaluation
-compile_time_operator = {
-    Operator.ADD:     operator.add,
-    Operator.SUB:     operator.sub,
-    Operator.DIV:     operator.div,
-    Operator.MOD:     operator.mod,
-    Operator.MUL:     operator.mul,
-    Operator.AND:     operator.and_,
-    Operator.OR:      operator.or_,
-    Operator.XOR:     operator.xor,
-    Operator.VAL2:    lambda a, b: b,
-    Operator.CMP_EQ:  operator.eq,
-    Operator.CMP_NEQ: operator.ne,
-    Operator.CMP_LT:  operator.lt,
-    Operator.CMP_GT:  operator.gt,
-    Operator.MIN:     lambda a, b: min(a, b),
-    Operator.MAX:     lambda a, b: max(a, b)
-}
-
-commutative_operators = [
-    Operator.ADD,
-    Operator.MUL,
-    Operator.AND,
-    Operator.OR,
-    Operator.XOR,
-    Operator.CMP_EQ,
-    Operator.CMP_NEQ,
-    Operator.MIN,
-    Operator.MAX,
-]
-
-# note: id_dicts is a *list* of dictionaries or (dictionary, function)-tuples
-def reduce_expr(expr, id_dicts = []):
-    global compile_time_operator
-    if isinstance(expr, BinOp):
-        expr.expr1 = reduce_expr(expr.expr1, id_dicts)
-        expr.expr2 = reduce_expr(expr.expr2, id_dicts)
-        if isinstance(expr.expr1, ConstantNumeric) and isinstance(expr.expr2, ConstantNumeric):
-            return ConstantNumeric(compile_time_operator[expr.op](expr.expr1.value, expr.expr2.value))
-        simple_expr1 = isinstance(expr.expr1, ConstantNumeric) or isinstance(expr.expr1, Parameter) or isinstance(expr.expr1, Variable)
-        simple_expr2 = isinstance(expr.expr2, ConstantNumeric) or isinstance(expr.expr2, Parameter) or isinstance(expr.expr2, Variable)
-        if expr.op in commutative_operators and ((simple_expr1 and not simple_expr2) or (isinstance(expr.expr2, Variable) and isinstance(expr.expr1, ConstantNumeric))):
-            expr.expr1, expr.expr2 = expr.expr2, expr.expr1
-        if isinstance(expr.expr1, Variable) and isinstance(expr.expr2, ConstantNumeric):
-            if expr.op == Operator.AND and isinstance(expr.expr1.mask, ConstantNumeric):
-                expr.expr1.mask = ConstantNumeric(expr.expr1.mask.value & expr.expr2.value)
-                return expr.expr1
-            if expr.op == Operator.ADD and expr.expr1.div == None and expr.expr1.mod == None:
-                if expr.expr1.add == None: expr.expr1.add = expr.expr2
-                else: expr.expr1.add = ConstantNumeric(expr.expr1.add + expr.expr2.value)
-                return expr.expr1
-            if expr.op == Operator.SUB and expr.expr1.div == None and expr.expr1.mod == None:
-                if expr.expr1.add == None: expr.expr1.add = ConstantNumeric(-expr.expr2.value)
-                else: expr.expr1.add = ConstantNumeric(expr.expr1.add - expr.expr2.value)
-                return expr.expr1
-            if expr.op == Operator.DIV and expr.expr1.div == None and expr.expr1.mod == None:
-                if expr.expr1.add == None: expr.expr1.add = ConstantNumeric(0)
-                expr.expr1.div = expr.expr2
-                return expr.expr1
-            if expr.op == Operator.MOD and expr.expr1.div == None and expr.expr1.mod == None:
-                if expr.expr1.add == None: expr.expr1.add = ConstantNumeric(0)
-                expr.expr1.mod = expr.expr2
-                return expr.expr1
-    elif isinstance(expr, Parameter):
-        expr.num = reduce_expr(expr.num, id_dicts)
-    elif isinstance(expr, Variable):
-        expr.num = reduce_expr(expr.num, id_dicts)
-        expr.shift = reduce_expr(expr.shift, id_dicts)
-        expr.mask = reduce_expr(expr.mask, id_dicts)
-        expr.param = reduce_expr(expr.param, id_dicts)
-    elif isinstance(expr, str):
-        for id_dict in id_dicts:
-            id_d, func = (id_dict, lambda x: ConstantNumeric(x)) if not isinstance(id_dict, tuple) else id_dict
-            if expr in id_d:
-                return func(id_d[expr])
-        raise ScriptError("Unrecognized identifier '" + expr + "' encountered")
-    return expr
-
-def reduce_constant(expr, id_dicts = []):
-    expr = reduce_expr(expr, id_dicts)
-    if not isinstance(expr, ConstantNumeric):
-        raise ConstError()
-    return expr
 
 ########### code blocks ###########
 class GRF:
