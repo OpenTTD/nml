4d78f4fd676cd3fdac0ec22935471ff80abbbd11
Yexo <yexo@openttd.org>
2010-06-19 01:35:14 +0200

    Fix: the name of action0 properties can be a ConstantNumeric but never an int

--- nml/parser.py
+++ nml/parser.py
@@ -234,9 +234,10 @@ class NMLParser(object):
                                | NUMBER COLON expression UNIT SEMICOLON
                                | NUMBER COLON string SEMICOLON
                                | NUMBER COLON array SEMICOLON'''
+        name = t[1] if isinstance(t[1], expression.Identifier) else expression.ConstantNumeric(t[1])
         val = expression.Array(t[3], t[1].pos) if isinstance(t[3], list) else t[3]
         unit = None if len(t) == 5 else ast.Unit(t[4])
-        t[0] = ast.Property(t[1], val, unit, t.lineno(1))
+        t[0] = ast.Property(name, val, unit, t.lineno(1))
 
     def p_graphics_block(self, t):
         'graphics_block : GRAPHICS LBRACE graphics_list RBRACE'
