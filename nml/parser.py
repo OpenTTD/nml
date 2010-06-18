90eb4a7a249065937ec1fc7bb926e9d5b1421943
Yexo <yexo@openttd.org>
2010-06-19 01:11:55 +0200

    Add: position tracking for the switch-blocks

--- nml/parser.py
+++ nml/parser.py
@@ -300,7 +300,7 @@ class NMLParser(object):
     #
     def p_switch(self, t):
         'switch : SWITCH LPAREN expression COMMA ID COMMA ID COMMA expression RPAREN LBRACE switch_body RBRACE'
-        t[0] = ast.Switch(t[3], t[5], t[7], t[9], t[12])
+        t[0] = ast.Switch(t[3], t[5], t[7], t[9], t[12], t.lineno(1))
 
     def p_switch_body(self, t):
         'switch_body : switch_ranges switch_value'
