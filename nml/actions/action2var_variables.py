f8e3d0965660b6607af806eb27a1e5f5f6aa450b
planetmaker <planetmaker@openttd.org>
2010-06-20 21:55:07 +0200

    Fix (r432): Better name and proper size for depot build date variable

--- nml/actions/action2var_variables.py
+++ nml/actions/action2var_variables.py
@@ -60,7 +60,7 @@ varact2vars_railtype = {
 	'terrain_type' : {'var': 0x40, 'start': 0, 'size': 8},
 	'enhanced_tunnels': {'var': 0x41, 'start': 0, 'size': 8},
 	'level_crossing_status': {'var': 0x42, 'start': 0, 'size': 8},
-	'days_since_0': {'var': 0x43, 'start': 0, 'size': 8},
+	'build_date': {'var': 0x43, 'start': 0, 'size': 32},
 }
 
 varact2vars_airporttiles = {
