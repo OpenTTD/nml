165fcd0429862dbd929b2d9663f7ad90da5b21d9
Hirundo <hirundo@openttdcoop.org>
2010-07-31 16:48:44 +0200

    Doc: Document ActionA.

--- nml/actions/actionA.py
+++ nml/actions/actionA.py
@@ -1,6 +1,12 @@
 from nml.actions import base_action, real_sprite
 
 class ActionA(base_action.BaseAction):
+    """
+    Action class for Action A (sprite replacement)
+
+    @ivar sets: List of sprite collections to be replaced
+    @type sets: C{list} of (C{int}, L{ConstantNumeric})-tuples
+    """
     def __init__(self, sets):
         self.sets = sets
 
@@ -17,6 +23,12 @@ class ActionA(base_action.BaseAction):
         file.end_sprite()
 
 def parse_actionA(replaces):
+    """
+    Parse replace-block to ActionA.
+
+    @param replaces: Replace-block to parse.
+    @type  replaces: L{ReplaceSprite}
+    """
     real_sprite_list = real_sprite.parse_sprite_list(replaces.sprite_list, replaces.pcx)
 
     return [ActionA([(len(real_sprite_list), replaces.start_id)])] + real_sprite_list
