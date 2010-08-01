69676d2e10f8f78cd247b380804dc4340ba8ae88
Hirundo <hirundo@openttdcoop.org>
2010-08-01 14:19:22 +0200

    Revert (r596): Make Assignment available globally again to promote code reuse.

--- nml/ast/grf.py
+++ nml/ast/grf.py
@@ -47,12 +47,3 @@ class GRF(object):
             ret += '\tdesc: %s;\n' % str(self.desc)
         ret += '}\n'
         return ret
-
-class Assignment(object):
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    def debug_print(self, indentation):
-        print indentation*' ' + 'Assignment, name = ', self.name
-        self.value.debug_print(indentation + 2)
