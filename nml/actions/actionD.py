576d5d1be0fa2e19f6dc23a20603c32a73a8d29e
Hirundo <hirundo@openttdcoop.org>
2010-10-10 21:05:22 +0200

    Codechange: Split parse_actionD into different parts.

--- nml/actions/actionD.py
+++ nml/actions/actionD.py
@@ -61,55 +61,140 @@ def get_tmp_parameter(expr):
     actions = parse_actionD(ParameterAssignment(expression.ConstantNumeric(param), expr))
     return (param, actions)
 
+def parse_ternary_op(assignment):
+    assert isinstance(assignment.value, expression.TernaryOp)
+    actions = parse_actionD(ParameterAssignment(assignment.param, assignment.value.expr2))
+    cond_block = nml.ast.conditional.Conditional(assignment.value.guard, [ParameterAssignment(assignment.param, assignment.value.expr1)], None, None)
+    actions.extend(cond_block.get_action_list())
+    return actions
+
+def parse_special_check(assignment):
+    assert isinstance(assignment.value, expression.SpecialCheck)
+    actions = parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(0)))
+    actions.append(nml.actions.action7.SkipAction(9, 0, 4, assignment.value.op, assignment.value.value, 1))
+    actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(1))))
+    return actions
+
+def parse_hasbit(assignment):
+    assert isinstance(assignment.value, expression.BinOp) and assignment.value.op == nmlop.HASBIT
+    actions = parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(0)))
+    cond_block = nml.ast.conditional.Conditional(assignment.value, [ParameterAssignment(assignment.param, expression.ConstantNumeric(1))], None, None)
+    actions.extend(cond_block.get_action_list())
+    return actions
+
+def parse_min_max(assignment):
+    assert isinstance(assignment.value, expression.BinOp) and assignment.value.op in (nmlop.MIN, nmlop.MAX)
+    #min(a, b) ==> a < b ? a : b.
+    #max(a, b) ==> a > b ? a : b.
+    action6.free_parameters.save()
+    action_list = []
+    expr1 = parse_subexpression(assignment.value.expr1, action_list)
+    expr2 = parse_subexpression(assignment.value.expr2, action_list)
+    guard = expression.BinOp(nmlop.CMP_LT if assignment.value.op == nmlop.MIN else nmlop.CMP_GT, expr1, expr2)
+    action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.TernaryOp(guard, expr1, expr2, None))))
+    action6.free_parameters.restore()
+    return action_list
+
+def parse_boolean(assignment):
+    assert isinstance(assignment.value, expression.Boolean)
+    actions = parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(0)))
+    expr = expression.BinOp(nmlop.CMP_NEQ, assignment.value.expr, expression.ConstantNumeric(0))
+    cond_block = nml.ast.conditional.Conditional(expr, [ParameterAssignment(assignment.param, expression.ConstantNumeric(1))], None, None)
+    actions.extend(cond_block.get_action_list())
+    return actions
+
+def transform_bin_op(assignment):
+    op = assignment.value.op
+    expr1 = assignment.value.expr1
+    expr2 = assignment.value.expr2
+    extra_actions = []
+
+    if op == nmlop.CMP_GE:
+        expr1, expr2 = expr2, expr1
+        op = nmlop.CMP_LE
+
+    if op == nmlop.CMP_LE:
+        extra_actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
+        op = nmlop.CMP_LT
+        expr1 = expression.Parameter(assignment.param)
+        expr2 = expression.ConstantNumeric(1)
+
+    if op == nmlop.CMP_GT:
+        expr1, expr2 = expr2, expr1
+        op = nmlop.CMP_LT
+
+    if op == nmlop.CMP_LT:
+        extra_actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
+        op = nmlop.SHIFTU_LEFT #shift left by negative number = shift right
+        expr1 = expression.Parameter(assignment.param)
+        expr2 = expression.ConstantNumeric(-31)
+
+    elif op == nmlop.CMP_NEQ:
+        extra_actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
+        op = nmlop.DIV
+        # We rely here on the (ondocumented) behavior of both OpenTTD and TTDPatch
+        # that expr/0==expr. What we do is compute A/A, which will result in 1 if
+        # A != 0 and in 0 if A == 0
+        expr1 = expression.Parameter(assignment.param)
+        expr2 = expression.Parameter(assignment.param)
+
+    elif op == nmlop.CMP_EQ:
+        # We compute A==B by doing not(A - B) which will result in a value != 0
+        # if A is equal to B
+        extra_actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
+        # Clamp the value to 0/1, see above for details
+        extra_actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.DIV, expression.Parameter(assignment.param), expression.Parameter(assignment.param)))))
+        op = nmlop.SUB
+        expr1 = expression.ConstantNumeric(1)
+        expr2 = expression.Parameter(assignment.param)
+
+    if op == nmlop.SHIFT_RIGHT or op == nmlop.SHIFTU_RIGHT:
+        if isinstance(expr2, expression.ConstantNumeric):
+            expr2.value *= -1
+        else:
+            expr2 = expression.BinOp(nmlop.SUB, expression.ConstantNumeric(0), expr2)
+        op = nmlop.SHIFT_LEFT if op == nmlop.SHIFT_RIGHT else nmlop.SHIFTU_LEFT
+
+    elif op == nmlop.XOR:
+        #a ^ b ==> (a | b) - (a & b)
+        expr1 = parse_subexpression(expr1, extra_actions)
+        expr2 = parse_subexpression(expr2, extra_actions)
+        tmp_param1, tmp_action_list1 = get_tmp_parameter(expression.BinOp(nmlop.OR, expr1, expr2))
+        tmp_param2, tmp_action_list2 = get_tmp_parameter(expression.BinOp(nmlop.AND, expr1, expr2))
+        extra_actions.extend(tmp_action_list1)
+        extra_actions.extend(tmp_action_list2)
+        expr1 = expression.Parameter(expression.ConstantNumeric(tmp_param1))
+        expr2 = expression.Parameter(expression.ConstantNumeric(tmp_param2))
+        op = nmlop.SUB
+
+    return op, expr1, expr2, extra_actions
+
 def parse_actionD(assignment):
     assignment.value.supported_by_actionD(True)
 
     if isinstance(assignment.value, expression.TernaryOp):
-        actions = parse_actionD(ParameterAssignment(assignment.param, assignment.value.expr2))
-        cond_block = nml.ast.conditional.Conditional(assignment.value.guard, [ParameterAssignment(assignment.param, assignment.value.expr1)], None, None)
-        actions.extend(cond_block.get_action_list())
-        return actions
+        return parse_ternary_op(assignment)
 
     if isinstance(assignment.value, expression.SpecialCheck):
-        actions = parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(0)))
-        actions.append(nml.actions.action7.SkipAction(9, 0, 4, assignment.value.op, assignment.value.value, 1))
-        actions.extend(parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(1))))
-        return actions
+        return parse_special_check(assignment)
 
     if isinstance(assignment.value, expression.BinOp):
         op = assignment.value.op
         if op == nmlop.HASBIT:
-            actions = parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(0)))
-            cond_block = nml.ast.conditional.Conditional(assignment.value, [ParameterAssignment(assignment.param, expression.ConstantNumeric(1))], None, None)
-            actions.extend(cond_block.get_action_list())
-            return actions
-
+            return parse_hasbit(assignment)
         elif op == nmlop.MIN or op == nmlop.MAX:
-            #min(a, b) ==> a < b ? a : b.
-            #max(a, b) ==> a > b ? a : b.
-            action6.free_parameters.save()
-            action_list = []
-            expr1 = parse_subexpression(assignment.value.expr1, action_list)
-            expr2 = parse_subexpression(assignment.value.expr2, action_list)
-            guard = expression.BinOp(nmlop.CMP_LT if op == nmlop.MIN else nmlop.CMP_GT, expr1, expr2)
-            action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.TernaryOp(guard, expr1, expr2, None))))
-            action6.free_parameters.restore()
-            return action_list
+            return parse_min_max(assignment)
 
     if isinstance(assignment.value, expression.Boolean):
-        actions = parse_actionD(ParameterAssignment(assignment.param, expression.ConstantNumeric(0)))
-        expr = expression.BinOp(nmlop.CMP_NEQ, assignment.value.expr, expression.ConstantNumeric(0))
-        cond_block = nml.ast.conditional.Conditional(expr, [ParameterAssignment(assignment.param, expression.ConstantNumeric(1))], None, None)
-        actions.extend(cond_block.get_action_list())
-        return actions
+        return parse_boolean(assignment)
 
     if isinstance(assignment.value, expression.Not):
         expr = expression.BinOp(nmlop.SUB, expression.ConstantNumeric(1), assignment.value.expr)
-        return parse_actionD(ParameterAssignment(assignment.param, expr))
+        assignment = ParameterAssignment(assignment.param, expr)
 
     if isinstance(assignment.value, expression.BinNot):
         expr = expression.BinOp(nmlop.SUB, expression.ConstantNumeric(0xFFFFFFFF), assignment.value.expr)
-        return parse_actionD(ParameterAssignment(assignment.param, expr))
+        assignment = ParameterAssignment(assignment.param, expr)
 
     action6.free_parameters.save()
     action_list = []
@@ -154,67 +239,8 @@ def parse_actionD(assignment):
         param2 = expression.ConstantNumeric(0xFE)
         data = expression.ConstantNumeric(assignment.value.grfid)
     elif isinstance(assignment.value, expression.BinOp):
-        op = assignment.value.op
-        expr1 = assignment.value.expr1
-        expr2 = assignment.value.expr2
-
-        if op == nmlop.CMP_GE:
-            expr1, expr2 = expr2, expr1
-            op = nmlop.CMP_LE
-
-        if op == nmlop.CMP_LE:
-            action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
-            op = nmlop.CMP_LT
-            expr1 = expression.Parameter(assignment.param)
-            expr2 = expression.ConstantNumeric(1)
-
-        if op == nmlop.CMP_GT:
-            expr1, expr2 = expr2, expr1
-            op = nmlop.CMP_LT
-
-        if op == nmlop.CMP_LT:
-            action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
-            op = nmlop.SHIFTU_LEFT #shift left by negative number = shift right
-            expr1 = expression.Parameter(assignment.param)
-            expr2 = expression.ConstantNumeric(-31)
-
-        elif op == nmlop.CMP_NEQ:
-            action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
-            op = nmlop.DIV
-            # We rely here on the (ondocumented) behavior of both OpenTTD and TTDPatch
-            # that expr/0==expr. What we do is compute A/A, which will result in 1 if
-            # A != 0 and in 0 if A == 0
-            expr1 = expression.Parameter(assignment.param)
-            expr2 = expression.Parameter(assignment.param)
-
-        elif op == nmlop.CMP_EQ:
-            # We compute A==B by doing not(A - B) which will result in a value != 0
-            # if A is equal to B
-            action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.SUB, expr1, expr2))))
-            # Clamp the value to 0/1, see above for details
-            action_list.extend(parse_actionD(ParameterAssignment(assignment.param, expression.BinOp(nmlop.DIV, expression.Parameter(assignment.param), expression.Parameter(assignment.param)))))
-            op = nmlop.SUB
-            expr1 = expression.ConstantNumeric(1)
-            expr2 = expression.Parameter(assignment.param)
-
-        if op == nmlop.SHIFT_RIGHT or op == nmlop.SHIFTU_RIGHT:
-            if isinstance(expr2, expression.ConstantNumeric):
-                expr2.value *= -1
-            else:
-                expr2 = expression.BinOp(nmlop.SUB, expression.ConstantNumeric(0), expr2)
-            op = nmlop.SHIFT_LEFT if op == nmlop.SHIFT_RIGHT else nmlop.SHIFTU_LEFT
-
-        elif op == nmlop.XOR:
-            #a ^ b ==> (a | b) - (a & b)
-            expr1 = parse_subexpression(expr1, action_list)
-            expr2 = parse_subexpression(expr2, action_list)
-            tmp_param1, tmp_action_list1 = get_tmp_parameter(expression.BinOp(nmlop.OR, expr1, expr2))
-            tmp_param2, tmp_action_list2 = get_tmp_parameter(expression.BinOp(nmlop.AND, expr1, expr2))
-            action_list.extend(tmp_action_list1)
-            action_list.extend(tmp_action_list2)
-            expr1 = expression.Parameter(expression.ConstantNumeric(tmp_param1))
-            expr2 = expression.Parameter(expression.ConstantNumeric(tmp_param2))
-            op = nmlop.SUB
+        op, expr1, expr2, extra_actions = transform_bin_op(assignment)
+        action_list.extend(extra_actions)
 
         if isinstance(expr1, expression.ConstantNumeric):
             param1 = expression.ConstantNumeric(0xFF)
