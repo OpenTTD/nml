d99105fb696adfd20714777c82bab4603160ebcd
planetmaker <planetmaker@openttd.org>
2011-10-03 19:40:40 +0200

    Add: New OpenTTD GUI sprites

--- nml/actions/action5.py
+++ nml/actions/action5.py
@@ -60,7 +60,7 @@ action5_table = {
     'AQUEDUCTS' : (0x12, 8, Action5BlockType.FIXED),
     'AUTORAIL' : (0x13, 55, Action5BlockType.FIXED),
     'FLAGS' : (0x14, 36, Action5BlockType.OFFSET),
-    'OTTD_GUI' : (0x15, 154, Action5BlockType.OFFSET),
+    'OTTD_GUI' : (0x15, 162, Action5BlockType.OFFSET),
     'AIRPORT_PREVIEW' : (0x16, 9, Action5BlockType.OFFSET),
 }
 
