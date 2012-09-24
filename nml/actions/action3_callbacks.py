acbabe32b29ffe86987e124d1aca3b4834de7c64
planetmaker <planetmaker@openttd.org>
2012-09-25 01:17:07 +0200

    Fix: Missing purchase_range callback

--- nml/actions/action3_callbacks.py
+++ nml/actions/action3_callbacks.py
@@ -113,7 +113,8 @@ callbacks[0x03] = {
     'mail_capacity' : {'type': 'cb', 'num': 0x36, 'var10': 0x11, 'purchase': 'purchase_mail_capacity'},
     'purchase_mail_capacity' : {'type': 'cb', 'num': 0x36, 'var10': 0x11, 'purchase': 2},
     'cargo_age_period' : {'type': 'cb', 'num': 0x36, 'var10': 0x1C},
-    'range' : {'type': 'cb', 'num': 0x36, 'var10': 0x1F},
+    'range' : {'type': 'cb', 'num': 0x36, 'var10': 0x1F, 'purchase': 'purchase_range'},
+    'purchase_range' : {'type': 'cb', 'num': 0x36, 'var10': 0x1F, 'purchase': 2},
     'rotor' : {'type': 'override'},
 }
 callbacks[0x03].update(general_vehicle_cbs)
