3dbc11c90dfa248e8e86fbf6cbb82f97c25fd690
Alberth <alberth@openttd.org>
2014-04-12 23:10:18 +0200

    Fix: Typo fixes in an error message.

--- nml/ast/sort_vehicles.py
+++ nml/ast/sort_vehicles.py
@@ -37,7 +37,7 @@ class SortVehicles(base_statement.BaseStatement):
     def pre_process(self):
         self.vehid_list = self.vehid_list.reduce(global_constants.const_list)
         if not isinstance(self.vehid_list, expression.Array) or not all([isinstance(x, expression.ConstantNumeric) for x in self.vehid_list.values]):
-            raise generic.ScriptError("Second parameter is not an array of one of the items in it could not be reduced to a constnat numer", self.pos)
+            raise generic.ScriptError("Second parameter is not an array of one of the items in it could not be reduced to a constant number", self.pos)
 
     def debug_print(self, indentation):
         print indentation*' ' + 'Sort, feature', hex(self.feature.value)
