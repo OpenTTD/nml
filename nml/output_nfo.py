52e9a9efb60d2798791372d4527c46f9a71a27d0
Alberth <alberth@openttd.org>
2014-05-31 11:34:55 +0200

    Fix: Printing Unicode characters in NFO was broken. Also added regression for it.

--- nml/output_nfo.py
+++ nml/output_nfo.py
@@ -82,7 +82,7 @@ class OutputNFO(output_base.SpriteOutputBase):
         if not grfstrings.is_ascii_string(value):
             if force_ascii:
                 raise generic.ScriptError("Expected ascii string but got a unicode string")
-            self.file.write('\xC3\x9E'.decode('utf-8'))
+            self.file.write('Þ') # b'\xC3\x9E'.decode('utf-8')
         self.file.write(value.replace('"', '\\"'))
         self.byte_count += grfstrings.get_string_size(value, final_zero, force_ascii)
         self.file.write('" ')
