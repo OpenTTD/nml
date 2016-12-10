5d73c9568f1ac5f296ae683f1d25e8373b641652
frosch <frosch@openttd.org>
2016-12-10 14:50:54 +0100

    Add: Roadvehicle property 'road_type'.

--- nml/actions/action0properties.py
+++ nml/actions/action0properties.py
@@ -364,6 +364,7 @@ def roadveh_speed_prop(prop_info):
     return [prop08, prop15]
 
 properties[0x01] = {
+    'road_type'                    : {'size': 1, 'num': 0x05},
     'speed'                        : roadveh_speed_prop({'unit_type': 'speed', 'unit_conversion': (10000, 1397), 'adjust_value': lambda val, unit: ottd_display_speed(val, 2, unit)}),
     'running_cost_factor'          : {'size': 1, 'num': 0x09},
     'running_cost_base'            : {'size': 4, 'num': 0x0A},
