cc99b91d5dbf673e09cac26966c8057a0e4fba4b
frosch <frosch@openttd.org>
2017-02-25 19:14:02 +0100

    Fix: If not roadtype- or tramtypetable is provided, OpenTTD defaults to the old ROAD and ELRL.

--- nml/global_constants.py
+++ nml/global_constants.py
@@ -1212,12 +1212,19 @@ def create_spritegroup_ref(info, pos):
     return expression.SpriteGroupRef(expression.Identifier(info), [], pos)
 
 cargo_numbers = {}
+
 is_default_railtype_table = True
+# if no railtype_table is provided, OpenTTD assumes these 4 railtypes
 railtype_table = {'RAIL': 0, 'ELRL': 1, 'MONO': 1, 'MGLV': 2}
+
 is_default_roadtype_table = True
-roadtype_table = {'ROAD': 0, 'ELRD': 1}
+# if no roadtype_table is provided, OpenTTD sets all vehicles to ROAD
+roadtype_table = {'ROAD': 0}
+
 is_default_tramtype_table = True
-tramtype_table = {'RAIL': 0, 'ELRL': 1}
+# if no tramtype_table is provided, OpenTTD sets all vehicles to ELRL
+tramtype_table = {'ELRL': 0}
+
 item_names = {}
 settings = {}
 named_parameters = {}
