62442e56c3059c9bdb30564b7bd1638155507d5e
glx22 <glx@openttd.org>
2020-11-16 00:59:36 +0100

    Cleanup: unused import

--- nml/actions/action3_callbacks.py
+++ nml/actions/action3_callbacks.py
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public License along
 with NML; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."""
 
-from nml import expression, nmlop
+from nml import nmlop
 
 callbacks = 0x14 * [{}]
 
