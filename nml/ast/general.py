67c497daa64ea94b2c4d12e5707bbc0c1bc4e27d
frosch <frosch@openttd.org>
2020-12-03 21:10:13 +0100

    Codechange: improve error message.

--- nml/ast/general.py
+++ nml/ast/general.py
@@ -54,7 +54,7 @@ def feature_name(feature):
     for name, num in feature_ids.items():
         if num == feature:
             return name
-    raise AssertionError("Invalid feature number")
+    raise AssertionError("Invalid feature number '{}'.".format(feature))
 
 
 def parse_feature(expr):
