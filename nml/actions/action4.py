e44e6798994389547703adaf7f5eb8ab94e906d1
glx22 <glx@openttd.org>
2023-05-28 04:23:08 +0200

    Change: extend DCxx string range up to FFFF

--- nml/actions/action4.py
+++ nml/actions/action4.py
@@ -82,7 +82,11 @@ string_ranges = {
     0xD0: {"random_id": True, "total": 0x400, "ids": list(range(0xD3FF, 0xCFFF, -1))},
     # Misc. persistent text ids, used to set properties.
     # Use Ids DC00..DCFF first to keep compatibility with older versions of OTTD.
-    0xDC: {"random_id": True, "total": 0x800, "ids": list(range(0xDBFF, 0xD7FF, -1)) + list(range(0xDFFF, 0xDBFF, -1))},
+    0xDC: {
+        "random_id": True,
+        "total": 0x2800,
+        "ids": list(range(0xFFFF, 0xDFFF, -1)) + list(range(0xDBFF, 0xD7FF, -1)) + list(range(0xDFFF, 0xDBFF, -1)),
+    },
 }
 
 # Mapping of string identifiers to D0xx/DCxx text ids
