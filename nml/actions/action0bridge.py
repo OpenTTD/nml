99ea323f8716eefaec8797977cab85194ac3cab2
Francisco Modesto <fran.bcn@gmail.com>
2026-04-29 01:08:26 +0100

    Example

--- nml/actions/action0bridge.py
+++ nml/actions/action0bridge.py
@@ -1,4 +1,4 @@
-﻿__license__ = """
+__license__ = """
 NML is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
